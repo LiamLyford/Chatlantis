26ae3abbb3015aeed1e998d17cad8bd210b4c69a
ElephantOfKnowledge <47584249+ElephantOfKnowledge@users.noreply.github.com>
2019-04-07 14:37:26 -0700

    functioning chat

--- Assignment/Chat/index.js
+++ Assignment/Chat/index.js
@@ -5,11 +5,15 @@ var io = require('socket.io')(http);
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var socketCookies = require('socket.io-cookie-parser');
+var fs = require('fs'); 
 
 app.use(cookieParser());
 app.use(session({secret: 'secret'}));
 io.use(socketCookies());
 
+//FOR MULTIPLE ROOMS USE SOCKET NAME SPACES
+
+//FOR CHAT LOGS MAKE CHAT OBJECTS FOR EACH MESSAGE
 
 // app.use((req, res, next) => {
 //   var randomIndex = Math.floor(Math.random() * testUsers.length);
@@ -17,9 +21,40 @@ io.use(socketCookies());
 //   next();
 // });
 
+// class message {
+//   constructor(msg, userID, timeStamp) {
+//     this.msg = msg;
+//     this.userID = userID;
+//     this.timeStamp = timeStamp;
+//   };
+// };
+
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+// var resetLogs = () => {
+//   var filestart = []
+//   db = './logs.json';
+//   fs.writeFileSync(db, JSON.stringify(filestart));
+//   console.log("Data reset!");
+// }
+
+// resetLogs();
+
+// var logMessage = (msg, user, time) => {
+//   db = './logs.json'; 
+//   logs = JSON.parse(fs.readFileSync(db)); 
+//   var log = {
+//     message: msg,
+//     userID: user,
+//     timeStamp: time
+//   };
+//   logs.push(log);
+//   fs.writeFileSync(db, JSON.stringify(logs));
+// }
 
 io.on('connection', (socket) => {
   // user = socket.request.cookies.name;
@@ -28,8 +63,10 @@ io.on('connection', (socket) => {
   // socket.on('disconnect', () => {
   //   io.emit('disconnect');
   // });
-  socket.on('chat message', (msg, user) => {
-    io.emit('chat message', msg, user);
+  console.log('User connected!')
+  socket.on('chat message', (msg, user, time) => {
+    io.emit('chat message', msg, user, time);
+    // logMessage(msg, user, time);
   });
 });
 
