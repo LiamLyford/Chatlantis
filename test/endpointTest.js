834e86551167bc9356bd2a020c53c1e8a7f3d627
Karl Schultze <karl.schultze@gmail.com>
2019-05-13 14:31:51 -0700

    Fixed endpoint tests

--- test/endpointTest.js
+++ test/endpointTest.js
@@ -4,31 +4,43 @@ const chaiHttp= require('chai-http');
 var should = chai.should();
 
 chai.use(chaiHttp);
-
+var website = chai.request('http://localhost:8080');
 describe('Testing Page Render',function(){
     it('Port test',function(){
-        chai.request('http://localhost:8080').get('/').end(function(err,res){
+        wait(1000);
+        website.get('/').end(function(err,res){
             res.should.have.status(200);
         });
     });
     it('Login test', function(){
-        chai.request('http://localhost:8080').get('/login').end(function(err,res){
+        wait(1000);
+        website.get('/login').end(function(err,res){
             res.should.have.status(200);
         })
     });
     it('Sign Up',function(){
-        chai.request('http://localhost:8080').get('/signup').end(function(err,res) {
+        wait(1000);
+        website.get('/signup').end(function(err,res) {
             res.should.have.status(200);
         });
     });
     it('Chatroom',function() {
-        chai.request('http://localhost:8080').get('/chatroom').end(function (err, res) {
+        wait(1000);
+        website.get('/chatroom').end(function (err, res) {
             res.should.have.status(200);
         });
     });
     it('Profile',function(){
-       chai.request('http://localhost:8080/profile').get('/chatroom').end(function(err,res){
+        wait(1000);
+       website.get('/profile/navi').end(function(err,res){
            res.should.have.status(200);
        }) ;
     });
 });
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
