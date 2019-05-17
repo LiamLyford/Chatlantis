3f98bd98902b7ec789e9006d67c1bab030653dc2
ElephantOfKnowledge <47584249+ElephantOfKnowledge@users.noreply.github.com>
2019-05-17 12:20:37 -0700

    added test?

---
+++ test/urlTest.js
@@ -0,0 +1,63 @@
+var supertest = require('supertest');
+const express = require('express');
+const chai = require('chai');
+const uuid = require('uuid');
+const request = require('supertest');
+const chaiHttp = require('chai-http');
+const app = express();
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+const website = chai.request('http://localhost:8080');
+describe('Testing Page Render', function () {
+    it('/', function () {
+        website.get('/').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        })
+    })
+    it('/login', function () {
+        website.get('/login').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        })
+    });
+    it('/signup', function () {
+        website.get('/signup').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        });
+    });
+    it('/chatroom', function () {
+        website.get('/chatroom').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        });
+    });
+    it('/profile', function () {
+        website.get('/profile/navi').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        });
+    });
+    it('/logout', function () {
+        website.get('/').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        })
+    })
+    it('/login/incorrect', function () {
+        website.get('/login/incorrect').end(function (err, res) {
+            expect(res).to.have.status(200);
+            // expect(err).to.be.null;
+
+        });
+    });
+    it('/login/signup/exists', function () {
+        website.get('/signup/exists').end(function (err, res) {
+            // expect(err).to.be.null;
+            expect(res).to.have.status(200);
+        })
+    });
+});
